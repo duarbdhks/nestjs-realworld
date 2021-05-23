2e8e5187a013da252ed13f7aff6283e3e60f6ea4
guwhan.yeum <guwhan.yeum@flitto.com>
2021-05-23 23:18:54 +0900

    태그 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Connection } from 'typeorm'
 import { AppController } from './app.controller';
 import { typeormConfig } from './ormconfig'
+import { TagModule } from './tag/tag.module'
 import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeormConfig),
-    UserModule
+    UserModule,
+    TagModule,
+    ProfileModule
   ],
   controllers: [AppController,],
   providers: [],
